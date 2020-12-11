ddeb243b58631ee4337a5ea917c89d4dc1eea3a4
dogsc729 <b07611033@ntu.edu.tw>
2020-12-11 16:50:34 +0800

    testing data input

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
+// **************************************************************************
+//  File       [main.cpp]
+//  Author     [Shu-Yu Lin]
+//  Synopsis   [The main program of 2020 fall Algorithm PA3]
+// **************************************************************************
+
+#include <cstring>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <bits/stdc++.h>
+#include <new>
+#include <ctime>
+#include <map>
 
 using namespace std;
-int main()
+void help_message()
 {
-    cout << "fuck" << endl;
+    cout << "usage: ./cb <input file name> <output file name>" << endl;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        help_message();
+        return 0;
+    }
+    fstream fin(argv[1]);
+    fstream fout;
+    fout.open(argv[2], ios::out);
+    char identifier;
+    fin.close();
+    fout.close();
     return 0;
 }
