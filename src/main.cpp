63e5a26acf8277ee73c6c4ce34a933610d54f324
dogsc729 <b07611033@ntu.edu.tw>
2021-01-16 22:05:52 +0800

    directed done(?)

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@
 #include <ctime>
 #include <map>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 struct Edge
@@ -44,6 +45,35 @@ int findroot(vector<int> root, int x)
     }
     return x;
 }
+
+bool cycle_detect_BFS(int s, int n, vector<vector<int>> adj_list)
+{
+    bool colored[n]; //number of vertices,colored or not
+    for (int i = 0; i < n; i++)
+        colored[i] = false; //initialize
+    queue<int> Q;
+    colored[s] = true;
+    Q.push(s);
+    while (!Q.empty())
+    {
+        int u = Q.front();
+        Q.pop();
+        for (int i = 0; i < adj_list[u].size(); i++)
+        {
+            if (colored[adj_list[u][i]])
+            {
+                return true;
+            }
+            else
+            {
+                colored[adj_list[u][i]] = true;
+                Q.push(adj_list[u][i]);
+            }
+        }
+    }
+    return false;
+}
+
 void kruskal_u(vector<Edge> &arr, int m, fstream &fout)
 {
     int n = arr.size();
@@ -78,12 +108,12 @@ void kruskal_u(vector<Edge> &arr, int m, fstream &fout)
         fout << removed_edges[i].start_v << " " << removed_edges[i].end_v << " " << removed_edges[i].weight << endl;
     }
 }
-
 void kruskal_d(vector<Edge> &arr, int m, fstream &fout)
 {
     int n = arr.size();
-    int removed_weight = 0;
     vector<Edge> removed_edges;
+    vector<Edge> remained_edges;
+    vector<Edge> test_edges;
     cout << m << " " << n << endl;
     sort(arr.begin(), arr.end(), compare);
     vector<int> root(m);
@@ -100,12 +130,76 @@ void kruskal_d(vector<Edge> &arr, int m, fstream &fout)
         if (rootstart != rootend)
         {
             root[rootstart] = rootend;
+            remained_edges.push_back({arr[i].start_v, arr[i].end_v, arr[i].weight});
         }
         else
         {
             removed_edges.push_back({arr[i].start_v, arr[i].end_v, arr[i].weight});
-            removed_weight = removed_weight + arr[i].weight;
         }
+    }
+    sort(removed_edges.begin(), removed_edges.end(), compare);
+    vector<vector<int>> adj_list(m);
+    //make a adjacency list for remaining edges
+    for (int i = 0; i < remained_edges.size(); i++)
+    {
+        adj_list[remained_edges[i].start_v].push_back(remained_edges[i].end_v);
+        //cout << remained_edges[i].start_v << " " << remained_edges[i].end_v << endl;
+    }
+    /*for (int i = 0; i < adj_list.size(); i++)
+    {
+        cout << i << endl;
+        for (int j = 0; j < adj_list[i].size(); j++)
+        {
+            cout << adj_list[i][j] << " ";
+        }
+        cout << endl;
+    }*/
+    //check whether each removed edges can be inserted into the graph
+    for (int i = 0; i < removed_edges.size(); i++)
+    {
+        //add adjacency list
+        adj_list[removed_edges[i].start_v].push_back(removed_edges[i].end_v);
+        //cout << removed_edges[i].start_v << endl;
+        //add the removed edge into the remaining edges
+        remained_edges.push_back(removed_edges[i]);
+        //check if cycle exist
+        if (cycle_detect_BFS(removed_edges[i].start_v, m, adj_list) == true)
+        //remove testing edges
+        {
+            //cout << "fuck" << endl;
+            //捨棄那個edge
+            remained_edges.pop_back();
+            //移除adjlist
+            adj_list[removed_edges[i].start_v].pop_back();
+        }
+        //keep testing edges
+        else
+        {
+            removed_edges[i].start_v = 0;
+            removed_edges[i].end_v = 0;
+            removed_edges[i].weight = 0;
+            //cout << "hello" << endl;
+        }
+    }
+    int removed_weight = 0;
+    for (int i = 0; i < removed_edges.size(); i++)
+    {
+        if (removed_edges[i].start_v != 0 && removed_edges[i].end_v != 0 && removed_edges[i].weight != 0)
+        {
+            removed_weight = removed_weight + removed_edges[i].weight;
+        }
+    }
+    fout << removed_weight << endl;
+    for (int i = 0; i < removed_edges.size(); i++)
+    {
+        if (removed_edges[i].start_v != 0 && removed_edges[i].end_v != 0 && removed_edges[i].weight != 0)
+        {
+            fout << removed_edges[i].start_v << " " << removed_edges[i].end_v << " " << removed_edges[i].weight << endl;
+        }
+    }
+    for (int i = 0; i < remained_edges.size(); i++)
+    {
+        cout << remained_edges[i].start_v << " " << remained_edges[i].end_v << endl;
     }
 }
 
@@ -129,7 +223,6 @@ int main(int argc, char *argv[])
     int num_of_edges;
     fin >> identifier >> num_of_vertices >> num_of_edges;
     vector<Edge> edges;
-    vector<bool> visited(num_of_vertices, false);
     if (identifier == "u")
     {
         for (int i = 0; i < num_of_edges; i++)
@@ -144,6 +237,7 @@ int main(int argc, char *argv[])
     }
     if (identifier == "d")
     {
+        vector<Edge> removed_edges;
         for (int i = 0; i < num_of_edges; i++)
         {
             int start_v;
@@ -152,6 +246,7 @@ int main(int argc, char *argv[])
             fin >> start_v >> end_v >> weight;
             edges.push_back({start_v, end_v, weight});
         }
+        kruskal_d(edges, num_of_vertices, fout);
     }
     fin.close();
     fout.close();
